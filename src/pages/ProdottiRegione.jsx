6155270a3217ced30db7693f1e9052e7bca08041
marcotristani <marco.tristani@hotmail.it>
2026-06-29 17:15:03 +0200

    fix errore

--- src/pages/ProdottiRegione.jsx
+++ src/pages/ProdottiRegione.jsx
@@ -52,7 +52,11 @@ function ProdottiRegione() {
         {prodottiTipici.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {prodottiTipici.map((prodotto) => (
-              <CardProdotto key={prodotto.id} prodotto={prodotto} />
+              <CardProdotto
+                key={prodotto.id}
+                prodotto={prodotto}
+                tipoProdotto={"prodottoTipico"}
+              />
             ))}
           </div>
         ) : (
--- src/pages/ViniRegione.jsx
+++ src/pages/ViniRegione.jsx
@@ -52,7 +52,11 @@ function ViniRegione() {
         {vini.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {vini.map((vino) => (
-              <CardProdotto key={vino.id} prodotto={vino} />
+              <CardProdotto
+                key={vino.id}
+                prodotto={vino}
+                tipoProdotto={"vino"}
+              />
             ))}
           </div>
         ) : (
